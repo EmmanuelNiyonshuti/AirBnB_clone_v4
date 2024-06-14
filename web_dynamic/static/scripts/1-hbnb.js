2d4610c5e2b5e28230e9e1f486e76bdba714d705
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-14 17:46:20 +0200

    1-hbnb.js solution

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,14 @@
-// $(function() {
-
-// });
+$(function() {
+  const objs = {}
+  $("input[type=checkbox]").on("change", function() {
+    const name = $(this).data('name')
+    const id = $(this).data('id')
+    if ($(this).is(":checked")) {
+      objs[id] = name
+    } else {
+      delete objs[id]
+    }
+    const ObjLis = Object.values(objs).join(", ");
+    $(".amenities h4").text(ObjLis)
+  });
+});
